917c1d4ab44e3b6c346e0ce5f134ff23f1247f41
stoledolarrain <74699a@nur.com.bo>
2023-10-12 18:04:27 -0400

    Project of Sistemas Operativos

--- src/main/java/WindowOne.java
+++ src/main/java/WindowOne.java
@@ -1,2 +1,22 @@
-public class WindowOne {
+import javax.swing.*;
+
+public class WindowOne extends JFrame {
+
+
+    public WindowOne(){
+        super("Registro");
+        setSize(600,600);
+        setResizable(true);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        WindowOne window1 = new WindowOne();
+        window1.setVisible(true);
+    }
 }

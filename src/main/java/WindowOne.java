e59e5ef71d575ba8d3355fbc1b65231cdd43eb06
Ernesto2001PO <ernestopazoliva@gmail.com>
2023-11-06 18:19:36 -0400

    Probando si funciona el commit prueba definitiva

--- src/main/java/WindowOne.java
+++ src/main/java/WindowOne.java
@@ -4,7 +4,7 @@ public class WindowOne extends JFrame {
 
 
     public WindowOne(){
-        super("Registro");
+        super("Registro para Baile");
         setSize(600,600);
         setResizable(true);
         setLocationRelativeTo(null);
@@ -12,6 +12,7 @@ public class WindowOne extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Hola pa esto es una prueba de git
         //Si ves esto hay que hacer un commit
+        //Y un push
 
         //22,
 

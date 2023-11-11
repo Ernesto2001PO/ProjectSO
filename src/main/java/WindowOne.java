793a3b3f6f88a259c92d3019264786559f46fb80
Ernesto2001PO <143664425+ErnestoNur@users.noreply.github.com>
2023-11-11 14:53:39 -0400

    Avance para el registro del baile

--- src/main/java/WindowOne.java
+++ src/main/java/WindowOne.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 
 public class WindowOne extends JFrame {
-
-
     public WindowOne(){
         super("Registro para Baile");
         setSize(600,600);
@@ -10,12 +8,42 @@ public class WindowOne extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Hola pa esto es una prueba de git
-        //Si ves esto hay que hacer un commit
-        //Y un push
-        //este cambio
+        JLabel label = new JLabel("Registro para Baile");
+        label.setBounds(250, 10, 200, 30);
+        add(label);
+        JLabel label2 = new JLabel("Nombre del chico");
+        label2.setBounds(10, 50, 200, 30);
+        add(label2);
+        JTextField textField = new JTextField();
+        textField.setBounds(10, 80, 200, 30);
+        add(textField);
+        JLabel label3 = new JLabel("Nombre de la chica");
+        label3.setBounds(10, 120, 200, 30);
+        add(label3);
+        JTextField textField2 = new JTextField();
+        textField2.setBounds(10, 150, 200, 30);
+        add(textField2);
+        JLabel label4 = new JLabel("Apellido");
+        label4.setBounds(10, 190, 200, 30);
+        add(label4);
+        JTextField textField3 = new JTextField();
+        textField3.setBounds(10, 220, 200, 30);
+        add(textField3);
+        JLabel label5 = new JLabel("Carnet");
+        label5.setBounds(10, 260, 200, 30);
+        add(label5);
+        JTextField textField4 = new JTextField();
+        textField4.setBounds(10, 290, 200, 30);
+        add(textField4);
+        JLabel label6 = new JLabel("Fecha de registro");
+        label6.setBounds(10, 330, 200, 30);
+        add(label6);
+        JTextField textField5 = new JTextField();
+        textField5.setBounds(10, 360, 200, 30);
+        add(textField5);
 
-        //22,
+
+
 
 
 

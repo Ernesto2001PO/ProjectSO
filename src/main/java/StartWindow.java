4fbaa52fab75d1995296a15ffd8c24d88f900ef2
stoledolarrain <samirtoledolarrain@gmail.com>
2023-11-30 07:40:48 -0400

    Samir

--- src/main/java/StartWindow.java
+++ src/main/java/StartWindow.java
@@ -5,22 +5,28 @@ import java.awt.event.ActionListener;
 public class StartWindow extends JFrame {
     public StartWindow(){
         super("Bienvenidos a Technologie Club");
-        setSize(600, 600);
+        setSize(1000, 600);
         setResizable(true);
         setLocationRelativeTo(null);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //Creando el background
+        ImageIcon logoPrueba = new ImageIcon(StartWindow.class.getResource("logoejemplo1.png"));
+        JLabel labellogoPrueba = new JLabel(logoPrueba);
+        labellogoPrueba.setBounds(0,0,1000,600);
+
+
         JButton registrarButton = new JButton("Registrar");
-        registrarButton.setBounds(250, 400, 100, 30);
+        registrarButton.setBounds(425, 400, 100, 30);
         registrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 openWindowOne();
             }
         });
-
-        add(registrarButton);
+        labellogoPrueba.add(registrarButton);
+        add(labellogoPrueba);
     }
     private void openWindowOne(){
         WindowOne windowOne = new WindowOne();
